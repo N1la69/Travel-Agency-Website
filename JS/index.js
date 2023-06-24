16464e5824f73be760e0313f32751ed024ec2022
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2023-06-24 17:03:46 +0530

    new

--- JS/index.js
+++ JS/index.js
@@ -1,30 +1,30 @@
 // SHOW MENU //
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close');
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
-if(navToggle){
-    navToggle.addEventListener('click', ()=>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu');
     })
 }
-if(navClose){
-    navClose.addEventListener('click', ()=>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu');
     })
 }
 
 const navLink = document.querySelectorAll('.nav_link');
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu');
     navMenu.classList.remove('show-menu');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 // BACKGROUND HEADER //
-function scrollHeader(){
+function scrollHeader() {
     const header = document.getElementById('header');
-    if(this.scrollY >= 100) {
+    if (this.scrollY >= 100) {
         header.classList.add('scroll-header');
     } else {
         header.classList.remove('scroll-header');
