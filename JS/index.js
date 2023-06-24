f8d761374178b86913f72be335bb00cb9fa1fa8a
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2023-06-24 20:17:30 +0530

    new

--- JS/index.js
+++ JS/index.js
@@ -19,7 +19,7 @@ function linkAction() {
     const navMenu = document.getElementById('nav-menu');
     navMenu.classList.remove('show-menu');
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 // BACKGROUND HEADER //
 function scrollHeader() {
@@ -30,4 +30,9 @@ function scrollHeader() {
         header.classList.remove('scroll-header');
     }
 }
-window.addEventListener('scroll', scrollHeader);
+window.addEventListener('scroll', scrollHeader)
+
+// VIDEO //
+const videoFile = document.getElementById('video-file'),
+      videoButton = document.getElementById('video-button'),
+      videoIcon = document.getElementById('video-icon')
