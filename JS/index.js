9fc384ef71eb8620202ebdffa2ffb5f337a00b59
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2023-06-24 13:09:25 +0530

    new

---
+++ JS/index.js
@@ -0,0 +1,33 @@
+// SHOW MENU //
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+if(navToggle){
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu');
+    })
+}
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu');
+    })
+}
+
+const navLink = document.querySelectorAll('.nav_link');
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// BACKGROUND HEADER //
+function scrollHeader(){
+    const header = document.getElementById('header');
+    if(this.scrollY >= 100) {
+        header.classList.add('scroll-header');
+    } else {
+        header.classList.remove('scroll-header');
+    }
+}
+window.addEventListener('scroll', scrollHeader);
