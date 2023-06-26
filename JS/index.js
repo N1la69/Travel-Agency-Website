8b5ae969666c2d9009342c8472de0f855b609167
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2023-06-26 12:03:35 +0530

    new

--- JS/index.js
+++ JS/index.js
@@ -66,3 +66,47 @@ function scrollUp() {
     }
 }
 window.addEventListener('scroll', scrollUp);
+
+// SCROLL-SECTION ACTIVE LINK //
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive);
+
+// DARK/LIGHT THEME //
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-button')
+
+const getCurrentTheme = () => document.body.classList.contains('darkTheme') ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains('iconTheme') ? 'ri-moon-line' : 'ri-sun-line';
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme);
+    localStorage.setItem('selected-icon', getCurrentIcon);
+})
